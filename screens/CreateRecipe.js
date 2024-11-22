746ee574bae21b93587dcfcb789bc07159e2e14b
dai2125 <dai2125@students.fhv.at>
2024-11-22 13:32:52 +0100

    database service createRecipe(param), getRecipe(param), getAllRecipe(), createCategory(param), getCategory(param), getAllCategories()

--- screens/CreateRecipe.js
+++ screens/CreateRecipe.js
@@ -1,5 +1,9 @@
 import React, {useState} from 'react'
 import {View, Text, Button, StyleSheet, TextInput} from 'react-native'
+import DatabaseService from './DatabaseService';
+
+const databaseService = new DatabaseService();
+
 
 export default RecipeList = ({navigation, route}) => {
 
@@ -16,15 +20,31 @@ export default RecipeList = ({navigation, route}) => {
    }
 
    const handleSubmit = () => {
-    console.log(formData);
+    // console.log(formData);
+    
+    // databaseService.createRecipe(formData.category, formData.name, formData.ingredients, formData.instructions, formData.notice);
+    // databaseService.createRecipe('category 2', 'name 2', 'ingredients 2', 'instructions 2', 'notice 2');
+    // databaseService.createRecipe('category 3', 'name 3', 'ingredients 3', 'instructions 3', 'notice 3');
+
+    // console.log(databaseService.getRecipe('cake'));
+    // console.log(databaseService.getAllRecipes());
+
+    // databaseService.createCategory('Salad', 'salad/url');
+    // databaseService.createCategory('Dessert', 'dessert/url');
+    // databaseService.createCategory('MainDish', 'mainDish/url');
+
+    // console.log(databaseService.getCategory('Dessert'));
+    // console.log(databaseService.getAllCategories());
   };
+
+ 
 
 
     return (
         <View style={styles.containerView}> 
             <TextInput
         placeholder="Category Name"
-        onChangeText={(text) => setFormData({...formData, name: text})}
+        onChangeText={(text) => setFormData({...formData, category: text})}
         />
       <TextInput
         placeholder="Name"
@@ -32,15 +52,15 @@ export default RecipeList = ({navigation, route}) => {
       />
       <TextInput
         placeholder="Ingredients"
-        onChangeText={(text) => setFormData({...formData, name: text})}
+        onChangeText={(text) => setFormData({...formData, ingredients: text})}
       />
       <TextInput
         placeholder="Instructions"
-        onChangeText={(text) => setFormData({...formData, name: text})}
+        onChangeText={(text) => setFormData({...formData, instructions: text})}
       />
       <TextInput
         placeholder="Notice"
-        onChangeText={(text) => setFormData({...formData, name: text})}
+        onChangeText={(text) => setFormData({...formData, notice: text})}
       />
        <Button title="Submit" onPress={handleSubmit} />
 
--- screens/DatabaseService.js
+++ screens/DatabaseService.js
@@ -1,40 +1,86 @@
 import { createStore } from 'tinybase';
+import { Recipe } from './Recipe';
+import { Category } from './Category';
+
+const RECIPE = 'recipe';
+const CATEGORY = 'category';
 
 class DatabaseService {
     constructor() {
         this.store = createStore();
         this.store.setTablesSchema({
         recipe: {
-            id: { type: 'number' },
             category: { type: 'string' },
             name: { type: 'string' },
             ingredients: { type: 'string' },
-            preparation: { type: 'string' }, 
+            instructions: { type: 'string' }, 
             notice: { type: 'string' },
         },
         category: {
-            id: { type: 'number' },
             name: { type: 'string' },
             url: { type: 'string' }
         },
     });}
-
     
-    createRecipe(category, name, ingredients, preparation, notice) {
-        this.store.setRow('recipe', 'recipe1', { 
-                        id: '1', 
+    createRecipe(category, name, ingredients, instructions, notice) {
+        this.store.setRow(RECIPE, name, { 
+                        name: name,
                         category: category, 
-                        name: name, 
                         ingredients: ingredients, 
-                        preparation: preparation, 
+                        instructions: instructions, 
                         notice: notice });
     }
 
-    getRecipe() {
-        return this.store.getRow('recipe', 'recipe1');
+    getRecipe(value) {
+        const data = this.store.getRow(RECIPE, value);
+
+        const recipe = new Recipe(data.name, data.category, data.ingredients, data.instructions, data.notice);
+
+        return recipe;
+    }
+
+    getAllRecipes() {
+        const rowData = this.store.getTable(RECIPE);
+
+        const allRecipes = Object.values(rowData).map(data =>
+            new Recipe(
+                data.name,
+                data.category,
+                data.ingredients,
+                data.instructions,
+                data.notice
+            )
+        );
+
+        return allRecipes;
+    }
+
+    createCategory(name, url) {
+        this.store.setRow(CATEGORY, name, { 
+                        name: name,
+                        url: url });
+    }
+
+    getCategory(value) {
+        const data = this.store.getRow(CATEGORY, value);
+
+        const category = new Category(data.name, data.url);
+
+        return category;
+    }
+
+    getAllCategories() {
+        const rowData = this.store.getTable(CATEGORY);
+
+        const allCategories = Object.values(rowData).map(data =>
+            new Category(
+                data.name,
+                data.url
+            )
+        );
+
+        return allCategories;
     }
 }
 
-
 export default DatabaseService;
-// export { createRecipe };
